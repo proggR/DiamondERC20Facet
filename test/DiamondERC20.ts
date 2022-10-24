abe4fc1f52984da143de193925d2ff69f8134067
Brando <sgt.groucho@gmail.com>
2022-10-23 21:04:03 -0400

    Isolated ERC20 Facet and related interface, storage contracts, and Hardhat tasks/tests. Testing dfm.js/diamond.json against actual Github repos (prototype works on local FS copies)

---
+++ test/DiamondERC20.ts
@@ -0,0 +1,61 @@
+const { expect, assert } = require("chai");
+const {
+  get,
+  getSelectors,
+} = require('../../lib/diamond/diamond.ts')
+
+
+// REQUIRED: name set, and diamond/owner/count as params to runTest
+// NOTE: count should include base diamond count (so +3). will need -1 for address idx
+const name = "DiamondERC20Facet"
+let diamond
+let owner
+let count
+
+const runTest = (dAddress, dOwner,ctr) => {
+  diamond = dAddress
+  owner = dOwner
+  count = ctr
+  describe("DiamondERC20 (DS for standard, AS for app level data) Test",test)
+}
+
+const test = async() => {
+  let contractOwner
+  let diamondAddress
+  let addresses
+  let facet
+  let diamondLoupeFacet
+
+  before(async() => {
+    contractOwner = owner;
+    diamondAddress = diamond;
+    const Facet = await ethers.getContractFactory("DiamondERC20Facet")
+    facet = new ethers.Contract(diamondAddress,Facet.interface, contractOwner)
+    diamondLoupeFacet = await ethers.getContractAt('DiamondLoupeFacet', diamondAddress)
+    addresses = await diamondLoupeFacet.facetAddresses()
+  })
+
+  step('Should have a total supply larger than 0', async () => {
+    const signers =  await ethers.getSigners();
+    const signer = signers[0]
+    const tx = await facet.construct()
+    const total = await facet.totalSupply()
+    expect(total).to.be.greaterThan(0)
+  })
+
+  step('Should have total supply when calling balanceOf(signer) and totalSupply()', async () => {
+    const signers =  await ethers.getSigners();
+    const signer = signers[0]
+    const balance = await facet.balanceOf(signer.address)
+    const total = await facet.totalSupply()
+    expect(balance).to.be.equal(total)
+  })
+
+}
+
+const mockerc20 = () => {
+}
+
+exports.name = name
+exports.runTest = runTest
+module.export = mockerc20
